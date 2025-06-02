88bec10dc595a2a5514ab9422c1cda5646f9b688
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-06-02 13:12:05 +0300

    add base pagination

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 1387);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
 }
 bootstrap();
