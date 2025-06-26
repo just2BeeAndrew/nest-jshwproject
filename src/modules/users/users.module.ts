b36706df0b9c530d297ff7c0edee81fcc5471a86
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-06-26 22:37:35 +0300

    add auth.controller.ts and auth.service.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,14 +6,16 @@ import { UsersController } from './api/users.controller';
 import { UsersRepository } from './infrastructure/users.repository';
 import { UsersQueryRepository } from './infrastructure/query/users.query-repository';
 import { BcryptModule } from '../bcrypt/bcrypt.module';
+import { AuthController } from './api/auth.controller';
+import { AuthService } from './application/auth.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     BcryptModule
   ],
-  controllers: [UsersController],
-  providers: [UsersService, UsersRepository, UsersQueryRepository],
+  controllers: [UsersController, AuthController],
+  providers: [UsersService, UsersRepository, UsersQueryRepository, AuthService],
   exports: [UsersService, MongooseModule],
 })
 export class UsersModule {}
