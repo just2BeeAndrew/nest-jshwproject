2eb54f71dad2181eb72594874afa08b00ef811b8
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-06-28 20:50:53 +0300

    update auth.controller.ts

--- src/modules/users/api/auth.controller.ts
+++ src/modules/users/api/auth.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { AuthService } from '../application/auth.service';
+import { AuthQueryRepository } from '../infrastructure/query/auth.query-repository';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private auuthQueryRepository: AuthQueryRepository,
+  ) {}
+
+  @Post('login')
+  @HttpCode(200)
+  async login() {}
+
+  @Post('password-recovery')
+  @HttpCode(200)
+  async passwordRecovvery() {}
+
+  @Post('new-password')
+  @HttpCode(200)
+  async newPassword() {}
+
+  @Post('registration-confirmation')
+  @HttpCode(200)
+  async registrationConfirmation() {}
+
+  @Post('registration')
+  @HttpCode(200)
+  async registration() {}
+
+  @Post('registration-email-resending')
+  @HttpCode(200)
+  async registrationEmailResending() {}
+
+  @Get('me')
+  @HttpCode(200)
+  async me() {}
+}
