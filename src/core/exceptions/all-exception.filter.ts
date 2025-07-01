fb598b571a69b2dd22d6a7c59a67bf02f370dc5a
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-07-01 22:44:48 +0300

    add exception filter

---
+++ src/core/exceptions/all-exception.filter.ts
@@ -0,0 +1,16 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const message = exception.message || 'Unknown exception occurred.';
+    const status = HttpStatus.INTERNAL_SERVER_ERROR;
+    const responseBody = this.buildResponseBody(request.url, message)
+  }
+
+  private buildResponseBody(requestUrl: string, message: string) {}
+}
--- src/modules/users/api/input-dto/create-users.input-dto.ts
+++ src/modules/users/api/input-dto/create-users.input-dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, Length, Matches } from 'class-validator';
+
 export class CreateUserInputDto {
+  @Length(3, 10)
+  @Matches(/^[a-zA-Z0-9_-]*$/)
   login: string;
+
+  @Length(6, 20)
   password: string;
+
+  @IsEmail()
+  @Matches(/^[\w.-]+@([\w-]+\.)+[\w-]{2,}$/,{message: 'Invalid email format'})
   email: string;
 }
