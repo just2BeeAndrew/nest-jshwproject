bf1bac381d33d5dc1d4192100424a1ab33bbf0d5
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-07-02 12:09:12 +0300

    patch exception filter

--- src/core/exceptions/filters/all-exception.filter.ts
+++ src/core/exceptions/filters/all-exception.filter.ts
@@ -1,5 +1,8 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
 import {Request, Response} from 'express';
+import { ErrorResponseBody } from './error-response-body';
+import * as process from 'node:process';
+import { DomainExceptionCode } from './domain-exception-codes';
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
@@ -11,7 +14,23 @@ export class AllExceptionFilter implements ExceptionFilter {
     const message = exception.message || 'Unknown exception occurred.';
     const status = HttpStatus.INTERNAL_SERVER_ERROR;
     const responseBody = this.buildResponseBody(request.url, message)
+
+    response.status(status).json(responseBody);
   }
 
-  private buildResponseBody(requestUrl: string, message: string) {}
+  private buildResponseBody(
+    requestUrl: string,
+    message: string):ErrorResponseBody {
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    if (isProduction) {
+      return {
+        timestamp: new Date().toISOString(),
+        path: null,
+        message: 'Some error occurred',
+        extensions: [],
+        code: DomainExceptionCode.InternalServerError,
+      };
+    }
+  }
 }
