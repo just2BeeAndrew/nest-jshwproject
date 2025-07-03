05cc041c4b88314eed574c250dedee2fb0608ef8
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-07-03 21:32:00 +0300

    patch exception filter

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -2,9 +2,11 @@ import {pipesSetup } from './pipes.setup';
 import {INestApplication} from '@nestjs/common';
 import {globalPrefixSetup} from './global-prefix.setup';
 import {swaggerSetup} from './swagger.setup';
+import { globalFiltersSetup } from './global-filters.setup';
 
 export function appSetup(app: INestApplication) {
   pipesSetup(app);
   globalPrefixSetup(app)
+  globalFiltersSetup(app)
   swaggerSetup(app)
 }
