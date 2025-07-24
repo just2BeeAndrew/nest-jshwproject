7f5c7a1dd606f67a2f506d33247ae83aa05b35b9
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-07-24 21:40:48 +0300

    update test

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -1,28 +1,53 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { appSetup } from '../src/setup/app.setup';
+import { Connection } from 'mongoose';
+import { getConnectionToken } from '@nestjs/mongoose';
+import { deleteAllData } from './helpers/delete-all-data';
+import { CreateUserInputDto } from '../src/modules/users/api/input-dto/create-users.input-dto';
+import request from 'supertest';
+import { GLOBAL_PREFIX } from '../src/setup/global-prefix.setup';
 
 describe('users', () => {
   let app: INestApplication;
+  let connection: Connection;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports:[AppModule]
+      imports: [AppModule],
     }).compile();
-    
+
     app = moduleFixture.createNestApplication();
-    
-    appSetup(app)
-    
-    await app.init()
+    appSetup(app);
+    await app.init();
+
+    connection = moduleFixture.get<Connection>(getConnectionToken());
+
+    await deleteAllData(app);
   });
-  
+
   afterEach(async () => {
     await app.close();
-  })
-
-  it('should ', () => {
-    
   });
-})
+
+  it('should create user', async () => {
+    const body: CreateUserInputDto = {
+      login: 'lg-197286',
+      password: 'string',
+      email: 'example@example.com',
+    };
+    const response = await request(app.getHttpServer())
+      .post(`/${GLOBAL_PREFIX}/users`)
+      .send(body)
+      .auth('admin', 'qwerty')
+      .expect(HttpStatus.CREATED)
+
+      expect(response.body).toEqual({
+        id:expect.any(String),
+        login: body.login,
+        email: body.email,
+        createdAt: expect.any(String),
+      })
+  });
+});
