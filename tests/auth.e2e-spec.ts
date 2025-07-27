9daf2f72efffdf3e9ff2929e2d71ab48c528d270
Andrew Dudal <andrew.dudal.1997@gmail.com>
2025-07-27 21:17:23 +0300

    update tests for auth

--- tests/auth.e2e-spec.ts
+++ tests/auth.e2e-spec.ts
@@ -1,11 +1,30 @@
 import { INestApplication } from '@nestjs/common';
 import { UsersTestManager } from './helpers/users-test-manager';
+import { initSettings } from './helpers/init-settings';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { deleteAllData } from './helpers/delete-all-data';
 
-describe('auth', ()=>{
+describe('auth', () => {
   let app: INestApplication;
   let usersTestManager: UsersTestManager;
 
   beforeAll(async () => {
-    const result = await 
+    const result = await initSettings((moduleBuilder) =>
+      moduleBuilder.overrideProvider(JwtService).useValue(
+        new JwtService({
+          secret: 'access-token-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ),
+    );
+    app = result.app;
+    usersTestManager = result.userTestManager
+  });
+
+  afterAll(async () => {
+    await app.close();
   })
-})
+  beforeEach(async () => {
+    await deleteAllData(app);
+  })
+});
